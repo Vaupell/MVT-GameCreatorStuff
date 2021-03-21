41d1439db43f3544ba57baf1f22b3d12ced1de1f
Martin Vaupell <martin@evisystems.org>
2021-03-21 19:06:32 +0100

    Create IgniterMVTKeyDown.cs

---
+++ IgniterMVTKeyDown.cs
@@ -0,0 +1,24 @@
+﻿namespace GameCreator.Core
+{
+	using System.Collections;
+	using System.Collections.Generic;
+	using UnityEngine;
+
+	[AddComponentMenu("")]
+	public class IgniterMVTKeyDown : Igniter 
+	{
+		public string UinputName;
+
+#if UNITY_EDITOR
+        public new static string NAME = "MVT/Unity Input";
+#endif
+
+		private void Update()
+		{
+			if (Input.GetButton(UinputName))
+			{
+                this.ExecuteTrigger(gameObject);
+			}
+		}
+	}
+}
