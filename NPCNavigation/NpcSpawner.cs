5eae85c9fc77b92d250f3f51f33474797faaf6d2
Martin Vaupell <2694539+Vaupell@users.noreply.github.com>
2021-05-10 17:53:05 +0200

    Update NpcSpawner.cs
    Respawn disable/enable via bool

--- NPCNavigation/NpcSpawner.cs
+++ NPCNavigation/NpcSpawner.cs
@@ -15,26 +15,45 @@ namespace GameCreator.Core
         // The main Spawnpoint array
         public string SpawnpointTagName = "NpcSpawnPoint";
         public GameObject[] SpawnPoints;
-        public bool[] SpInUse;
 
         // Npcs and max ammount
         public GameObject[] NpcsToSpawn; // Remember to add npc's in the inspector
         public string NpcTagName = "Enemy";
+        public bool RespawnNPCs = false;
         public int MaintainNpcAmmount = 3;
         public int CurrentNpcs = 0;
 
         // script internal use only
+        private bool initialized = false;
         private int _RndNpc;
         private int _RndSp;
 
         public override bool InstantExecute(GameObject target, IAction[] actions, int index)
         {
-            if (CurrentNpcs <= MaintainNpcAmmount)
+            // private initialization, run only once.
+            if (initialized == false)
             {
-                // find and setup spawnpoints
+                // setup array
                 SpawnPoints = GameObject.FindGameObjectsWithTag(SpawnpointTagName);
-                SpInUse = new bool[SpawnPoints.Length];
 
+                // initial spawns based on maintain npc ammount
+                for (int i = 0; i <= MaintainNpcAmmount; i++)
+                {
+                    // rnd spawn and npc selection
+                    _RndNpc = Random.Range(0, NpcsToSpawn.Length);
+                    _RndSp = Random.Range(0, SpawnPoints.Length);
+
+                    UpdateNpcs();
+                    SpawnNewNpc(_RndNpc, _RndSp);
+                }
+
+                initialized = true;
+            }
+
+
+            // normal check or "loop" by GC ;)
+            if (CurrentNpcs <= MaintainNpcAmmount && RespawnNPCs == true)
+            {
                 // select random range to select a NPC to spawn
                 _RndNpc = Random.Range(0, NpcsToSpawn.Length);
                 if (myDebug) Debug.Log("Random range found: " + _RndNpc);
@@ -45,12 +64,13 @@ namespace GameCreator.Core
 
                 // spawn new npc
                 SpawnNewNpc(_RndNpc, _RndSp);
-            } else
-            {
-                UpdateNpcs();
             }
+
+            // count Npcs in scene
+            UpdateNpcs();
             return true;
         }
+
 
         private void UpdateNpcs()
         {
@@ -59,8 +79,8 @@ namespace GameCreator.Core
 
         private void SpawnNewNpc(int Character, int SelectedSpawn)
         {
-            Instantiate(NpcsToSpawn[Character], SpawnPoints[SelectedSpawn].transform.position, Quaternion.Euler(0, 0, 0));
-            CurrentNpcs++;
+                Instantiate(NpcsToSpawn[Character], SpawnPoints[SelectedSpawn].transform.position, Quaternion.Euler(0, 0, 0));
+                CurrentNpcs++;
         }
 
 #if UNITY_EDITOR
